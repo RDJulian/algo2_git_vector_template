243c4a0e1bbabf7ef28cbb3a28e1d689903602cb
RDJulian <jruizd@fi.uba.ar>
2023-04-18 16:27:13 -0300

    feat: Vector template con popBack y pushBack.

--- src/Vector.h
+++ src/Vector.h
@@ -1,8 +1,140 @@
-//
-// Created by Julian on 17/4/2023.
-//
+#ifndef VECTOR_H
+#define VECTOR_H
 
-#ifndef ALGO2_GIT_VECTOR_TEMPLATE_VECTOR_H
-#define ALGO2_GIT_VECTOR_TEMPLATE_VECTOR_H
+#include <cstddef>
 
-#endif //ALGO2_GIT_VECTOR_TEMPLATE_VECTOR_H
+class IndiceNoValidoException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: el indice ingresado no es valido.";
+    }
+};
+
+class VectorVacioException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: no se puede eliminar el dato porque el vector esta vacio.";
+    }
+};
+
+template<typename Dato>
+class Vector {
+private:
+    Dato* datos;
+    size_t tamanio{};
+    size_t cantidadDatos{};
+
+    void copiarDatos();
+
+    void generarVector();
+
+    void ampliarVector();
+
+    void reducirVector();
+
+    void verificarTamanio();
+
+public:
+    Vector();
+
+    void pushBack(Dato dato);
+
+    Dato popBack();
+
+    size_t size();
+
+    Dato& operator[](size_t indice);
+
+    ~Vector();
+};
+
+template<typename Dato>
+Vector<Dato>::Vector() {
+    datos = nullptr;
+    tamanio = 0;
+    cantidadDatos = 0;
+}
+
+template<typename Dato>
+void Vector<Dato>::copiarDatos() {
+    Dato* nuevoVector = new Dato[tamanio];
+    for (size_t i = 0; i < cantidadDatos; i++) {
+        nuevoVector[i] = datos[i];
+    }
+    delete[] datos;
+    datos = nuevoVector;
+}
+
+template<typename Dato>
+void Vector<Dato>::generarVector() {
+    datos = new Dato[1];
+    tamanio = 1;
+}
+
+template<typename Dato>
+void Vector<Dato>::ampliarVector() {
+    tamanio *= 2;
+    copiarDatos();
+}
+
+template<typename Dato>
+void Vector<Dato>::reducirVector() {
+    if (cantidadDatos == 0) {
+        delete[] datos;
+        datos = nullptr;
+        tamanio = 0;
+    } else {
+        tamanio /= 2;
+        copiarDatos();
+    }
+}
+
+template<typename Dato>
+void Vector<Dato>::verificarTamanio() {
+    if (tamanio == 0) {
+        generarVector();
+    } else if (cantidadDatos == tamanio) {
+        ampliarVector();
+    } else if (cantidadDatos == tamanio / 2) {
+        reducirVector();
+    }
+}
+
+template<typename Dato>
+void Vector<Dato>::pushBack(Dato dato) {
+    verificarTamanio();
+    datos[cantidadDatos] = dato;
+    cantidadDatos++;
+}
+
+template<typename Dato>
+Dato Vector<Dato>::popBack() {
+    if (cantidadDatos == 0) {
+        throw VectorVacioException();
+    } else {
+        Dato dato = datos[--cantidadDatos];
+        verificarTamanio();
+        return dato;
+    }
+}
+
+template<typename Dato>
+size_t Vector<Dato>::size() {
+    return cantidadDatos;
+}
+
+template<typename Dato>
+Dato& Vector<Dato>::operator[](size_t indice) {
+    if (indice >= cantidadDatos) {
+        throw IndiceNoValidoException();
+    } else {
+        return datos[indice];
+    }
+}
+
+template<typename Dato>
+Vector<Dato>::~Vector() {
+    delete[] datos;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
-//
-// Created by Julian on 17/4/2023.
-//
+#include <iostream>
+#include "Vector.h"
+
+using namespace std;
+
+int main() {
+    Vector<int> vector;
+
+    for (int i = 1; i <= 5; i++) {
+        vector.pushBack(i * i);
+    }
+
+    for (size_t i = 0; i < vector.size(); i++) {
+        cout << vector[i] << endl;
+        vector[i] += 1;
+    }
+
+    while (vector.size()) {
+        cout << "Elimine el elemento: " << vector.popBack() << endl;
+    }
+
+    return 0;
+}
