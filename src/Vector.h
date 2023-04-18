2ba63aa87c40fa93cee3b2a58b044bbdbb8f7655
RDJulian <jruizd@fi.uba.ar>
2023-04-18 19:24:48 -0300

    Creacion de vector template y declaracion de los metodos.

--- src/Vector.h
+++ src/Vector.h
@@ -1,8 +1,27 @@
-//
-// Created by Julian on 17/4/2023.
-//
-
 #ifndef ALGO2_GIT_VECTOR_TEMPLATE_VECTOR_H
 #define ALGO2_GIT_VECTOR_TEMPLATE_VECTOR_H
 
-#endif //ALGO2_GIT_VECTOR_TEMPLATE_VECTOR_H
+template<typename generico>
+class Vector {
+private:
+    int tamanio;
+    int cantidad;
+    generico* elementos;
+
+public:
+    Vector();
+
+    void expandirVector();
+
+    void reducirVector();
+
+    void pushBack(generico algo);
+
+    generico popBack();
+
+    generico& elemento(int indice);
+
+    ~Vector();
+};
+
+#endif
